6748b98ea51a243294d6321543493230631f53b1
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-01 07:53:58 +0000

    deploy: 64d74e3f58c264bbf2c5124a0267a9c6be7455af

---
+++ channel.js
@@ -0,0 +1,12 @@
+var ChannelDef;
+(function(ChannelDef2) {
+  ChannelDef2[ChannelDef2["ch1"] = 0] = "ch1";
+  ChannelDef2[ChannelDef2["ch2"] = 1] = "ch2";
+})(ChannelDef || (ChannelDef = {}));
+const ch1Fx = (x) => void 0;
+const Channel = new Map([
+  [0, ch1Fx]
+]);
+const channel2 = {
+  [0]: ch1Fx
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "./_snowpack/pkg/react.js";
+import ReactDOM from "./_snowpack/pkg/react-dom.js";
+import {PreWsMessage} from "./lib/ws-message.js";
+import WsWrapper from "./lib/Wrapper.js";
+const url = "ws://localhost:5000";
+var WsChannel;
+(function(WsChannel2) {
+  WsChannel2["useCase1"] = "ch1";
+})(WsChannel || (WsChannel = {}));
+const w = new PreWsMessage(url);
+const App = () => {
+  w.sendMessage({channel: WsChannel.useCase1, data: {message: "string"}});
+  w.ws.onmessage = (x) => {
+    console.log(JSON.parse(x.data));
+  };
+  return /* @__PURE__ */ React.createElement("p", null, "Content");
+};
+ReactDOM.render(/* @__PURE__ */ React.createElement(WsWrapper, {
+  w
+}, /* @__PURE__ */ React.createElement(App, null)), document.getElementById("root"));
